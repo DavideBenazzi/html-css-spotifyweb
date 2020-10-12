adb01f37e7b03ed6fb7590e32e1db0539c8f8468
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-12 15:34:04 +0200

    Aggiunta Javascript + Modifiche Finali

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+/*
+Javascript
+SOSTITUZIONE IN FINESTRA SCROLLABILE DI TITOLO H3
+DA 'RECENTLY PLAYED' A 'POWERED BY JAVASCRIPT'
+*/
+document.getElementById('javascript').innerHTML = 'Powered by Javascript';
